7e22086eda49cbb8623edf592663975ccc0a75be
Michele Ceo <michele.ceo@tesmec.com>
2022-05-18 00:54:42 +0200

    - Cleaning

--- Log.cs
+++ Log.cs
@@ -9,40 +9,17 @@ namespace BlackJackSimul
 {
     public class Log
     {
-        //public void Log()
-        //{
-        //    if(Costanti.f_txtLog)
-
-
-        //}
-        // private FileStream fs = File.Create("Giocate");
-
-
-        // StringBuilder statistics = new StringBuilder();
-
         public void Write(string text)
         {
             if (Costanti.f_console)
                 Console.Write(text);
-
         }
 
         public void WriteLine(string text)
         {
             if (Costanti.f_console)
                 Console.WriteLine(text);
-
         }
 
-
-        //    statistics.AppendLine($"HL TrueCounter -" +
-        //                              $" MIN: {stat_data.memory.HL_TrueCountersequence.Min()}" +
-        //                              $" MAX: {stat_data.memory.HL_TrueCountersequence.Max()}");
-        //        statistics.AppendLine($"RAPC TrueCounter -" +
-        //                              $" MIN: {stat_data.memory.RAPC_TrueCounterSequence.Min()}" +
-        //                              $" MAX: {stat_data.memory.RAPC_TrueCounterSequence.Max()}");
-
-        //        File.WriteAllText("Stat.log", statistics.ToString());
-        //}
     }
 }
