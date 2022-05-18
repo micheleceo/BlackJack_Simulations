48092c3428c9bbd8b0aab38d811c0266aa370ceb
Michele Ceo <michele.ceo@tesmec.com>
2022-05-18 23:41:02 +0200

    - Added Configuration.json

---
+++ Config.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackSimul
+{
+
+    public class Config
+    {
+        public int ShoeDeckTotalNumber { get; set; }
+        public int ShoeDeckToExtractNumber { get; set; }
+        public int SimulationTotalShoes { get; set; }
+        public bool f_dealer_Soft17_hit { get; set; }
+        public bool f_console { get; set; }
+    }
+
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using BlackJackSimul.CountingStrategy;
 using BlackJackSimul.Data;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,76 +14,16 @@ namespace BlackJackSimul
         static void Main(string[] args)
         {
 
-           // CreateSeqeunceCSV();
+           // var conf = new Config();
+           // File.WriteAllText("Configuration.json", JsonConvert.SerializeObject(conf, Formatting.Indented));
+
+            Config conf = JsonConvert.DeserializeObject<Config>(File.ReadAllText("Configuration.json"));
+            string configJa = JsonConvert.SerializeObject(conf, Formatting.Indented);
 
             Simulation simulation = new Simulation(Costanti.N_SHOES, Costanti.N_MAZZI_PER_SHOE, Costanti.N_MAZZI_DA_ESTRARRE_PER_SHOE);
             simulation.Start();
 
         }
-
-
-        //public static void CreateSeqeunceCSV()
-        //{
-        //    // Single extraction data
-        //    var se_data = new SingleExstractionData();
-
-        //    // Statistic data
-        //    var stat_data = new StatisticData();
-
-        //    //Creazione del CSV per singola estrazione
-        //    FileCSV fileCSV = new FileCSV("Data.csv");
-        //    fileCSV.WriteHeader(";", se_data);
-
-        //    List<string> regularDeck = CardDeck.Create();
-
-        //    //Ciclo sugli shoe
-        //    for (int nShoes = 1; nShoes <= Costanti.N_SHOES; nShoes++)
-        //    {
-        //        // Statistic data
-        //        se_data = new SingleExstractionData();
-
-        //        // Creazione shoe
-        //        var shoe = new Shoe(Costanti.N_MAZZI_PER_SHOE, Costanti.N_MAZZI_DA_ESTRARRE_PER_SHOE, regularDeck);
-        //        shoe.Shuffle();
-
-        //        //Creazione dei counter
-        //        var hl_counter = new HL_Counter(shoe);
-        //        var rapc_counter = new RAPC_Counter(shoe);
-
-        //        se_data.ShoeNumber = nShoes;
-
-        //        //Ciclo delle estrazioni per shoe
-        //        for (int n = 1; n <= Costanti.N_CARTE_DA_ESTRARRE_PER_SHOE; n++)
-        //        {
-        //            if (n % Costanti.N_CARTE_MAZZO == 0)
-        //                se_data.deckNumber++;
-
-        //            //Prendi una carta
-        //            se_data.Card = shoe.GetCard();
-
-        //            //Aggiornamento counter HL
-        //            hl_counter.UpdateMainCounters(se_data.Card);
-        //            se_data.HL_RunningCounter = hl_counter.RunningCounter;
-        //            se_data.HL_TrueCounter = hl_counter.TrueCounter;
-        //            stat_data.memory.HL_TrueCountersequence.Add(hl_counter.TrueCounter);
-
-        //            //Aggiornamento counter RAPC
-        //            rapc_counter.UpdateMainCounters(se_data.Card);
-        //            se_data.RAPC_RunningCounter = rapc_counter.RunningCounter;
-        //            se_data.RAPC_TrueCounter = rapc_counter.TrueCounter;
-        //            stat_data.memory.RAPC_TrueCounterSequence.Add(rapc_counter.TrueCounter);
-
-        //            fileCSV.WriteLine(se_data);
-        //        }
-
-        //    }
-
-        //    fileCSV.Close();
-
-        //    WriteStatistic(stat_data);
-
-        //}
-
 
         //private static void WriteStatistic(StatisticData stat_data)
         //{
